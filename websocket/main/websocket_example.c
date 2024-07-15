ff3d5421e96cfa9e0b33a85a3a003fb2f9065ea8
sozib <sozibalmamun@yahoo.com>
2024-07-15 11:10:58 +0600

    dynamic arry for stomp and handle data sending

--- websocket/main/websocket_example.c
+++ websocket/main/websocket_example.c
@@ -1,136 +1,9 @@
-/* ESP HTTP Client Example
+#include "websocket_example.h"
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
+const char *TAG = "WEBSOCKET";
+const char *TAGSTOMP = "STOMP_CLIENT";
 
 
-#include <stdio.h>
-#include "esp_wifi.h"
-#include "esp_system.h"
-#include "nvs_flash.h"
-#include "esp_event.h"
-#include "protocol_examples_common.h"
-
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/semphr.h"
-#include "freertos/event_groups.h"
-
-#include "esp_log.h"
-#include "esp_websocket_client.h"
-#include "esp_event.h"
-
-
-// #define THT     "wss://grozziieget.zjweiting.com:3091/CloudSocket-Dev/websocket/" worked
-
-#define THT     "wss://grozziieget.zjweiting.com:3091/CloudSocket-Dev/websocket/"// testing 
-
-// #define HOST    "wss://grozziieget.zjweiting.com/"// testing
-#define HOST    "grozziieget.zjweiting.com"// done
-
-#define PORT    3091
-#define  PATH  "/CloudSocket-Dev/websocket/"
-
-#define STOMP_CONNECT_FRAME     "[\"CONNECT\\naccept-version:1.1\\nheart-beat:10000,10000\\n\\n\\u0000\"]"
-
-typedef struct {
-    const char                  *uri;                       /*!< Websocket URI, the information on the URI can be overrides the other fields below, if any */
-    const char                  *host;                      /*!< Domain or IP as string */
-    int                         port;                       /*!< Port to connect, default depend on esp_websocket_transport_t (80 or 443) */
-    const char                  *path;                       /*!< Websocket URI, the information on the URI can be overrides the other fields below, if any */
-} stompInfo_cfg_t;
-
-
-
-static const char *TAG = "example";
-
-esp_websocket_client_handle_t client;
-
-const char echo_org_ssl_ca_cert[]  = \
-"-----BEGIN CERTIFICATE-----\n" \
-"MIIFBzCCA++gAwIBAgIRALIM7VUuMaC/NDp1KHQ76aswDQYJKoZIhvcNAQELBQAw\n" \
-"ezELMAkGA1UEBhMCR0IxGzAZBgNVBAgMEkdyZWF0ZXIgTWFuY2hlc3RlcjEQMA4G\n" \
-"A1UEBwwHU2FsZm9yZDEaMBgGA1UECgwRQ29tb2RvIENBIExpbWl0ZWQxITAfBgNV\n" \
-"BAMMGEFBQSBDZXJ0aWZpY2F0ZSBTZXJ2aWNlczAeFw0yMjAxMTAwMDAwMDBaFw0y\n" \
-"ODEyMzEyMzU5NTlaMFkxCzAJBgNVBAYTAkNOMSUwIwYDVQQKExxUcnVzdEFzaWEg\n" \
-"VGVjaG5vbG9naWVzLCBJbmMuMSMwIQYDVQQDExpUcnVzdEFzaWEgUlNBIERWIFRM\n" \
-"UyBDQSBHMjCCAaIwDQYJKoZIhvcNAQEBBQADggGPADCCAYoCggGBAKjGDe0GSaBs\n" \
-"Yl/VhMaTM6GhfR1TAt4mrhN8zfAMwEfLZth+N2ie5ULbW8YvSGzhqkDhGgSBlafm\n" \
-"qq05oeESrIJQyz24j7icGeGyIZ/jIChOOvjt4M8EVi3O0Se7E6RAgVYcX+QWVp5c\n" \
-"Sy+l7XrrtL/pDDL9Bngnq/DVfjCzm5ZYUb1PpyvYTP7trsV+yYOCNmmwQvB4yVjf\n" \
-"IIpHC1OcsPBntMUGeH1Eja4D+qJYhGOxX9kpa+2wTCW06L8T6OhkpJWYn5JYiht5\n" \
-"8exjAR7b8Zi3DeG9oZO5o6Qvhl3f8uGU8lK1j9jCUN/18mI/5vZJ76i+hsgdlfZB\n" \
-"Rh5lmAQjD80M9TY+oD4MYUqB5XrigPfFAUwXFGehhlwCVw7y6+5kpbq/NpvM5Ba8\n" \
-"SeQYUUuMA8RXpTtGlrrTPqJryfa55hTuX/ThhX4gcCVkbyujo0CYr+Uuc14IOyNY\n" \
-"1fD0/qORbllbgV41wiy/2ZUWZQUodqHWkjT1CwIMbQOY5jmrSYGBwwIDAQABo4IB\n" \
-"JjCCASIwHwYDVR0jBBgwFoAUoBEKIz6W8Qfs4q8p74Klf9AwpLQwHQYDVR0OBBYE\n" \
-"FF86fBEQfgxncWHci6O1AANn9VccMA4GA1UdDwEB/wQEAwIBhjASBgNVHRMBAf8E\n" \
-"CDAGAQH/AgEAMB0GA1UdJQQWMBQGCCsGAQUFBwMBBggrBgEFBQcDAjAiBgNVHSAE\n" \
-"GzAZMA0GCysGAQQBsjEBAgIxMAgGBmeBDAECATBDBgNVHR8EPDA6MDigNqA0hjJo\n" \
-"dHRwOi8vY3JsLmNvbW9kb2NhLmNvbS9BQUFDZXJ0aWZpY2F0ZVNlcnZpY2VzLmNy\n" \
-"bDA0BggrBgEFBQcBAQQoMCYwJAYIKwYBBQUHMAGGGGh0dHA6Ly9vY3NwLmNvbW9k\n" \
-"b2NhLmNvbTANBgkqhkiG9w0BAQsFAAOCAQEAHMUom5cxIje2IiFU7mOCsBr2F6CY\n" \
-"eU5cyfQ/Aep9kAXYUDuWsaT85721JxeXFYkf4D/cgNd9+hxT8ZeDOJrn+ysqR7NO\n" \
-"2K9AdqTdIY2uZPKmvgHOkvH2gQD6jc05eSPOwdY/10IPvmpgUKaGOa/tyygL8Og4\n" \
-"3tYyoHipMMnS4OiYKakDJny0XVuchIP7ZMKiP07Q3FIuSS4omzR77kmc75/6Q9dP\n" \
-"v4wa90UCOn1j6r7WhMmX3eT3Gsdj3WMe9bYD0AFuqa6MDyjIeXq08mVGraXiw73s\n" \
-"Zale8OMckn/BU3O/3aFNLHLfET2H2hT6Wb3nwxjpLIfXmSVcVd8A58XH0g==\n" \
-"-----END CERTIFICATE-----\n";
-
-
-
-void stomp_client_connect() {
-
-    char connect_frame[100] ;//"[\"CONNECT\naccept-version:1.1\nheart-beat:10000,10000\n\n\u0000\"]";
-    snprintf(connect_frame, sizeof(connect_frame), "%s%s%s%s", "[\"CONNECT\\n", "accept-version:1.1\\n", "heart-beat:10000,10000\\n\\n", "\\u0000\"]");
-    ESP_LOGI(TAG, "Sending STOMP CONNECT frame:\n%s", connect_frame);
-    esp_websocket_client_send_text(client, connect_frame, sizeof(connect_frame), portMAX_DELAY);
-}
-void stomp_client_subscribe(char* topic) {
-    
-    //example pack "[\"SUBSCRIBE\\nid:sub-0\\ndestination:/topic/cloud\\nack:client\\n\\n\\u0000\"]";
-
-    char connect_frame[100] ;
-    snprintf(connect_frame, sizeof(connect_frame), "%s%s%s%s%s%s%s", "[\"SUBSCRIBE\\n","id:sub-0\\n", "destination:",topic,"\\n", "ack:client\\n\\n", "\\u0000\"]");
-
-    ESP_LOGI(TAG, "Sending STOMP Subscribe frame :\n%s", connect_frame);
-    esp_websocket_client_send_text(client, connect_frame, strlen(connect_frame), portMAX_DELAY);
-}
-void stompSend(char * buff, char* topic){
-
-    //example pac 
-    //char ["SEND\ndestination:/app/cloud\n\nESP\n\n\u0000"]
-
-    char connect_frame[512];
-    snprintf(connect_frame, sizeof(connect_frame), "%s%s%s%s%s%s%s", "[\"SEND\\n", "destination:",topic,"\\n\\n", buff,"\\n\\n", "\\u0000\"]");
-    ESP_LOGI(TAG, "Sending STOMP MSG :\n%s", connect_frame);
-    esp_websocket_client_send_text(client, connect_frame, strlen(connect_frame), portMAX_DELAY);
-}
-
-
-
-
-void stomp_client_handle_message( const char *message) {
-
-       ESP_LOGI("example", "Received STOMP message:\n%s", message);
-    if (strstr(message, "CONNECTED")) {
-        ESP_LOGI("example", "STOMP CONNECTED");
-        // Subscribe to a topic
-        stomp_client_subscribe("/topic/cloud");
-    } else if (strstr(message, "MESSAGE")) {
-        ESP_LOGI("example", "STOMP MESSAGE");
-        
-        stompSend(echo_org_ssl_ca_cert,"/app/cloud");
-
-        // Handle the received message
-    } else if (strstr(message, "ERROR")) {
-        ESP_LOGE("example", "STOMP ERROR: %s", message);
-    }
-}
 
 static void websocket_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
 {
@@ -138,15 +11,12 @@ static void websocket_event_handler(void *handler_args, esp_event_base_t base, i
     switch (event_id) {
     case WEBSOCKET_EVENT_CONNECTED:
         ESP_LOGI(TAG, "WEBSOCKET_EVENT_CONNECTED");
-
-        // stomp_client_connect();     
-
         break;
     case WEBSOCKET_EVENT_DISCONNECTED:
         ESP_LOGI(TAG, "WEBSOCKET_EVENT_DISCONNECTED");
+
         break;
     case WEBSOCKET_EVENT_DATA:
-        // ESP_LOGI(TAG, "Received opcode=%d", data->op_code);
         if (data->op_code == 0x08 && data->data_len == 2) {
             // ESP_LOGW(TAG, "Received closed message with code=%d", 256*data->data_ptr[0] + data->data_ptr[1]);
         } else {
@@ -155,13 +25,13 @@ static void websocket_event_handler(void *handler_args, esp_event_base_t base, i
             stomp_client_connect();            
             }  
             else if((char *)data->data_ptr[0]=='a'){
+
                 stomp_client_handle_message(&data->data_ptr[3]);
+
             }else if((char *)data->data_ptr[0]=='h'){
                 ESP_LOGI(TAG, "Ping");
             } 
         }
-        // ESP_LOGW(TAG, "Total payload length=%d, data_len=%d, current payload offset=%d\r\n", data->payload_len, data->data_len, data->payload_offset);
-
         break;
     case WEBSOCKET_EVENT_ERROR:
         ESP_LOGI(TAG, "WEBSOCKET_EVENT_ERROR");
@@ -170,8 +40,19 @@ static void websocket_event_handler(void *handler_args, esp_event_base_t base, i
 }
 
 
+
+void app_main(void)
+{
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    ESP_ERROR_CHECK(example_connect());
+
+    websocket_app_start();
+}
+
+
 void stomp_client_int( stompInfo_cfg_t stompSetup ) {
-   
    
     esp_websocket_client_config_t websocket_cfg = {};
 
@@ -196,7 +77,7 @@ void stomp_client_int( stompInfo_cfg_t stompSetup ) {
     // websocket_cfg.skip_cert_common_name_check = false;
     // websocket_cfg.disable_auto_reconnect = false;
 
-        websocket_cfg.use_global_ca_store = true;// ok 
+    websocket_cfg.use_global_ca_store = true;// ok 
     websocket_cfg.skip_cert_common_name_check = true;
     websocket_cfg.disable_auto_reconnect = false;
 
@@ -226,25 +107,77 @@ static void websocket_app_start(void)
     stomp_client_int(stompInfo);
 }
 
-void app_main(void)
-{
-    ESP_LOGI(TAG, "[APP] Startup..");
-    ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
-    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
-    esp_log_level_set("*", ESP_LOG_INFO);
-    esp_log_level_set("WEBSOCKET_CLIENT", ESP_LOG_DEBUG);
-    esp_log_level_set("TRANSPORT_WS", ESP_LOG_DEBUG);
-    esp_log_level_set("TRANS_TCP", ESP_LOG_DEBUG);
+void stomp_client_connect() {
 
-    ESP_ERROR_CHECK(nvs_flash_init());
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    char connect_frame[100] ;//"[\"CONNECT\naccept-version:1.1\nheart-beat:10000,10000\n\n\u0000\"]";
+    snprintf(connect_frame, sizeof(connect_frame), "%s%s%s%s", "[\"CONNECT\\n", "accept-version:1.1\\n", "heart-beat:10000,10000\\n\\n", "\\u0000\"]");
+    ESP_LOGI(TAGSTOMP, "Sending STOMP CONNECT frame:\n%s", connect_frame);
+    esp_websocket_client_send_text(client, connect_frame, strlen(connect_frame), portMAX_DELAY);
+}
+void stomp_client_subscribe(char* topic) {
+    
+    //example pack "[\"SUBSCRIBE\\nid:sub-0\\ndestination:/topic/cloud\\nack:client\\n\\n\\u0000\"]";
 
-    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
-     * Read "Establishing Wi-Fi or Ethernet Connection" section in
-     * examples/protocols/README.md for more information about this function.
-     */
-    ESP_ERROR_CHECK(example_connect());
+    char connect_frame[60+strlen(topic)] ;
+    snprintf(connect_frame, sizeof(connect_frame), "%s%s%s%s%s%s%s", "[\"SUBSCRIBE\\n","id:sub-0\\n", "destination:",topic,"\\n", "ack:client\\n\\n", "\\u0000\"]");
 
-    websocket_app_start();
+    ESP_LOGI(TAGSTOMP, "Sending STOMP Subscribe frame :%s\n", connect_frame);
+    esp_websocket_client_send_text(client, connect_frame, strlen(connect_frame), portMAX_DELAY);
+}
+bool stompSend(char * buff, char* topic){
+
+    //example pac 
+    //char ["SEND\ndestination:/app/cloud\n\nESP\n\n\u0000"]
+
+    char tempBuff[1024];
+    uint16_t buffLen= strlen(buff);
+    uint16_t tempLen=0;
+
+    do{
+
+        if(buffLen<=1024){
+            memcpy(&tempBuff,&buff,buffLen);
+        }else{
+            ESP_LOGI(TAGSTOMP, "Buffer more then 1kb: %d ", buffLen);
+            memcpy(&tempBuff,&buff[tempLen],1024);
+        }
+
+        char connect_frame[strlen(tempBuff)+strlen(topic)+36];
+        ESP_LOGI(TAGSTOMP, "connect frame len: %d ", sizeof(connect_frame));
+
+        snprintf(connect_frame, sizeof(connect_frame), "%s%s%s%s%s%s%s", "[\"SEND\\n", "destination:",topic,"\\n\\n", tempBuff,"\\n\\n", "\\u0000\"]");
+
+        ESP_LOGI(TAGSTOMP, "Sending STOMP MSG :\n%s", connect_frame);
+
+        if(!esp_websocket_client_is_connected(client)) return false;
+
+            if(esp_websocket_client_send_text(client, connect_frame, strlen(connect_frame), portMAX_DELAY)!=ESP_OK){
+                buffLen= buffLen-strlen(tempBuff); // calculate remaining data buff
+                tempLen+=strlen(tempBuff);// calculate starting ary addr at copy.
+                memset(tempBuff,0,strlen(tempBuff));
+            }
+
+    }while(buffLen!=0);
+    
+    return true;
+
+}
+
+
+void stomp_client_handle_message( const char *message) {
+
+    //    ESP_LOGI(TAGSTOMP, "Received STOMP message:\n%s", message);
+    if (strstr(message, "CONNECTED")) {
+        ESP_LOGI(TAGSTOMP, "STOMP CONNECTED");
+        // Subscribe to a topic
+        stomp_client_subscribe("/topic/cloud");
+    } else if (strstr(message, "MESSAGE")) {
+        ESP_LOGI(TAGSTOMP, "STOMP MESSAGE");
+        
+        stompSend(echo_org_ssl_ca_cert,"/app/cloud");
+
+        // Handle the received message
+    } else if (strstr(message, "ERROR")) {
+        ESP_LOGE(TAGSTOMP, "STOMP ERROR: %s", message);
+    }
 }
